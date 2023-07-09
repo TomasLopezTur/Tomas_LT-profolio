7ffbbcff08cf973da4b43b5fb72a6ee8149c0fc5
TomasLopezTur <tomas.lopez.tur@gmail.com>
2023-07-09 18:10:09 -0300

    agregando pagina porfolio

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const process = require("process");
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require("method-override");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3005;
+// Template engine
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: false}));
+
+// Routers 
+const indexRouter = require("./routes");
+
+//Routes Middlewares
+app.use("/", indexRouter);
+
+
+// Open PORT
+app.listen(PORT, () => console.log(`Server listen in port: ${PORT}\nhttp://localhost:${PORT}`));
